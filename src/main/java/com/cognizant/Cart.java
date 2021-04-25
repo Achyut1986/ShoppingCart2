bc01ecf0c32868efe05fd2363067286284672454
Achyut1986 <82679069+Achyut1986@users.noreply.github.com>
2021-04-25 15:50:55 -0500

    commit in shopping cart

---
+++ src/main/java/com/cognizant/Cart.java
@@ -0,0 +1,11 @@
+package com.cognizant;
+
+import java.util.List;
+
+public class Cart<Item> {
+    private int subTotal;
+    private List<Item> itemList;
+
+    public boolean isEmpty() {
+    }
+}
---
+++ src/test/java/com/cognizant/ShoppingCartLab.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShoppingCartLab {
+    @Test
+    void emptyCart(){
+    Cart cart= new Cart();
+    assertTrue(cart.isEmpty());
+    }
+}
